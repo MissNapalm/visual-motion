f0a07eaede8e28bd0e7d9ce3d612788a139d482a
MissNapalm <148738331+MissNapalm@users.noreply.github.com>
2025-10-04 19:20:17 -0500

    Initialize Electron app with main window setup

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      webviewTag: true,
+      webSecurity: false  // Allow loading MediaPipe from CDN
+    }
+  });
+
+  win.loadFile('index.html');
+  
+  // Open DevTools to see console errors
+  win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
